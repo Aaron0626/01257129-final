7bd5f5287783edd282b9c6bda09d8b5efeb705f1
Aaron0626 <dora9402@gmail.com>
2025-11-26 12:17:42 +0800

    Initial Commit

---
+++ 01257129-final/_1257129_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1257129_finalApp.swift
+//  01257129-final
+//
+//  Created by user10 on 2025/11/26.
+//
+
+import SwiftUI
+
+@main
+struct _1257129_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
